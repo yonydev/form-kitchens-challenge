62768e77e59e612afe4ddeac2ad933118f8bbbfe
Yonatan Rojas <yonatan.rojas@crehana.com>
2022-08-18 18:40:44 -0500

    refactor: created src folder to hold views, pages and styles folder

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className="w-3/4 px-20">
+      <h1 className="text-underlined">HOla Mundo</h1>
+    </div>
+  );
+};
+
+export default Home;
