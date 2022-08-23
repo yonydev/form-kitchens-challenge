0d82a9d621914ee2d174d7fa2c8766eb62dac183
Yonatan Rojas <yonatan.rojas@crehana.com>
2022-08-22 19:05:10 -0500

    feat: created EmptyState and SearchInput components

---
+++ src/views/Home/components/EmptyState/EmptyState.tsx
@@ -0,0 +1,8 @@
+const EmptyState = () => {
+  return (
+    <section className="h-full w-full flex items-center justify-center text-slate-400 text-2xl">
+      <p>Sorry! The product you are looking for does not exist 😢</p>
+    </section>
+  );
+};
+export default EmptyState;
---
+++ src/views/Home/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+
+type TSearchInput = {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+};
+
+const SearchInput: FC<TSearchInput> = ({ onChange, value }) => {
+  return (
+    <div className="text-gray-600">
+      <input
+        value={value}
+        onChange={onChange}
+        className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+        placeholder="Search"
+      ></input>
+    </div>
+  );
+};
+
+export default SearchInput;
